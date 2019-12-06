1c0fd7adbb32a36841fad815dc7b3b94781dc5cf
Eva Mutuku <mutileeva@gmail.com>
2019-12-06 06:43:38 -0800

    Center logo.

--- js/script.js
+++ js/script.js
@@ -1,5 +1,14 @@
 $(document).ready(function() {
+
   $("#design").click(function(){
+    $("p").toggle();
+  })
+
+  $("#development").click(function(){
+    $("p").toggle();
+  })
+
+  $("#productManagement").click(function(){
     $("p").toggle();
   })
 })
