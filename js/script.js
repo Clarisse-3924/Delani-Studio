03898f6ed45fbe15a8895d5e1d7590b48359fde2
Eva Mutuku <mutileeva@gmail.com>
2019-12-08 11:08:58 -0800

    Create submit function for contact form

--- js/script.js
+++ js/script.js
@@ -12,11 +12,23 @@ $(document).ready(function() {
     $("#product-management").toggle();
   })
 
-  $("#contact").submit(function(event) {
-    document.getElementById("contact").submit();
-    alert("We have received your message. Thank you for reaching out to us");
+  // $("#contact").submit(function(event) {
+  //   document.getElementById("contact").submit();
+  //   alert("We have received your message. Thank you for reaching out to us");
 
-    
+  $("#Contact-Us").submit(function (event) {
+    var name = $("input:name").val();
+    var email = $("input:email").val();
+    var message = $("input:comment").val();
+    if (name || email || message != '') {
+      alert("Thank you " + name + " your message is well received !");
+    } else {
+      alert('Invalid input');
+    }
+    event.preventDefault();
+  })
+});
+
     event.preventDefault();
   })
 
